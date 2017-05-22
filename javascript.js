93ec83d83e55a5e2252abf21a9c4f32ec4d29cf7
Mark <jungtaehoon@icloud.com>
2017-05-23 02:39:26 +0900

    ver 0.7

--- javascript.js
+++ javascript.js
@@ -6,21 +6,51 @@ var ageOfNextYear;
 
 var ageOfNext10Years;
 
-var friendList;
-
 var isMarried;
 
-
 if (isMarried) {
-  
+  var spouseName;
 } else {
-
+  var hasBoyOrGrildFriend;
+  if (hasBoyOrGrildFriend) {
+    var BoyOrGrildFriendName;
+  }
 }
 
+var friendList;
 
+var attachArrayElements = function(array) {
+  var sum = '';
+
+  //아래 for loop을 활용해 첫 번째 인자로 받은 array에 있는 요소들을 한 칸씩 띄어 하나의 문자열로 만듭니다.
+  for (var i = 0; i < '이 곳을 채워넣으세요'; i++) {
+    sum += ', ' + array[i];
+  }
+};
+
+//위의 attachArrayElements함수를 활용하세요
+var friendNames;
+
+var parents = {
+  mother: {
+    name: null,
+    birthday: null
+  },
+  father: {
+    name: null,
+    birthday: null
+  }
+};
 
 //아래 부분은 고치지 마세요.
-var aboutMe = `나의 이름은 ${name}다. 올해 나이는 ${age}이다.
-내년에는 ${ageOfNextYear}이고 10년 뒤에는 ${ageOfNext10Years}다`;
+var aboutMe = `나의 이름은 ${name}다. 
+  올해 나이는 ${age}이다.
+  내년에는 ${ageOfNextYear}이고 10년 뒤에는 ${ageOfNext10Years}다.
+  현재 결혼은 ${isMarried === true ? '' : isMarried === false ? '안' : '?'}했다.
+  나에게는 ${Array.isArray(friendList) ? friendList.length : 0}명의 친구들이 있다.
+  그들의 이름은 ${friendNames}(이)다.
+  어머니의 성함은 ${parents.mother.name}(이)고 ${parents.mother.birthday}에 태어나셨다.
+  아버지의 성함은 ${parents.father.name}(이)고 ${parents.father.birthday}에 태어나셨다.
+  `;
 
 document.getElementById('result').innerText = aboutMe;
--- spec/tests.js
+++ spec/tests.js
@@ -1,23 +1,60 @@
 describe('나의 소개', function() {
   it('age의 데이터 형은 숫자(number)이어야 합니다', function() {
     expect(age).to.be.a('number');
-  })
+  });
   it('name의 데이터 형은 문자열(string)이어야 합니다', function() {
     expect(string).to.be.a('string');
-  })
+  });
   it('ageOfNextYear(1년 뒤 나이)은 age보다 1이 커야 합니다', function() {
     expect(ageOfNextYear).to.equal(age + 1);
-  })
+  });
   it('ageOfNext10Years(10년 뒤 나이)은 age보다 10이 커야 합니다', function() {
     expect(ageOfNextYear).to.equal(age + 1);
-  })
-  it('friendList(친구들 리스트)의 데이터 형은 배열(array)이여야 합니다', function() {
-    expect(friendList).to.be.a('array');
-  })
-  it('friendList(친구들 리스트)는 한명 이상의 친구를 가져야 합니다', function() {
-    expect(friendList).to.not.lengthOf(0);
-  })  
+  });
   it('isMarried(결혼 했는지)의 데이터 형은 불리언(boolean)이어야 합니다', function() {
     expect(isMarried).to.be.a('boolean');
-  })
+  });
+  it('', function() {
+    expect();
+  });
+  if (isMarried === true) {
+    it('결혼했다면 spouseName(배우자 이름)의 데이터 형은 문자열(string)이어야 합니다', function() {
+      expect(spouseName).to.be.a('string');
+    });   
+  } else {
+    it('결혼 안했다면 hasBoyOrGrildFriend(이성친구 여부)의 데이터 형은 불리언(boolean)이어야 합니다', function() {
+      expect(hasBoyOrGrildFriend).to.be.a('string');
+    }); 
+  }
+  if (hasBoyOrGrildFriend) {
+    it('이성친구가 있다면 BoyOrGrildFriendName(이성친구의 이름)의 데이터 형은 문자열(string)이어야 합니다', function() {
+      expect(BoyOrGrildFriendName).to.be.a('string');
+    });
+  }
+  it('friendList(친구들 리스트)의 데이터 형은 배열(array)이여야 합니다', function() {
+    expect(friendList).to.be.a('array');
+  });
+  it('friendList(친구들 리스트)는 한명 이상의 친구를 가져야 합니다', function() {
+    expect(friendList).to.not.lengthOf(0);
+  });  
+  it('attachArrayElements(배열의 원소들 붙이기)는 문자열(string)을 반환해야 합니다', function() {
+    expect(attachArrayElements(['하', '호'])).to.be.a('string');
+  });
+  it('friendNames은 friendList(친구들 리스트)에 있는 모든 친구들의 이름을 \', \'로 이어붙인 문자열 이어야 합니다.  ', function() {
+    var sum = '';
+    for (var i = 0; i < friendList.length; i++) {
+      sum += ', ' + friendList[i];
+    }    
+    expect(friendNames).to.equal(sum);
+  });
+  for (var key in parents) {
+    it('부모님의 성함은 문자열(string)이어야 합니다', function() {
+      expect(parents[key].name).to.be.a('string');
+    });
+  }
+  for (var key in parents) {
+    it('부모님의 생신은 숫자(number)이어야 합니다', function() {
+      expect(parents[key].birthday).to.be.a('number');
+    });
+  }
 });
